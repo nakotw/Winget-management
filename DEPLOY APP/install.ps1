8e17b3b7cbc7bf0be18da7c641dce9b5ff63c94d
florian <74260396+nakotw@users.noreply.github.com>
2024-04-10 15:56:42 -0400

    Add files via upload

---
+++ DEPLOY APP/install.ps1
@@ -0,0 +1,21 @@
+﻿Param
+  (
+    [parameter(Mandatory=$false)]
+    [String[]]
+    $param
+  )
+  
+$ProgramName = "Adobe.Acrobat.Reader.64-bit"
+Start-Transcript -Path "$env:ProgramData\Microsoft\IntuneManagementExtension\Logs\$ProgramName-install.log" -Force
+
+# resolve winget_exe
+$winget_exe = Resolve-Path "C:\Program Files\WindowsApps\Microsoft.DesktopAppInstaller_*_x64__8wekyb3d8bbwe\winget.exe"
+if ($winget_exe.count -gt 1){
+        $winget_exe = $winget_exe[-1].Path
+}
+
+if (!$winget_exe){Write-Error "Winget not installed"}
+
+& $winget_exe install --exact --id $ProgramName --silent --accept-package-agreements --accept-source-agreements --scope=machine $param
+
+Stop-Transcript
