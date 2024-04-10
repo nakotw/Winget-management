c76f49ae7801d4953c8f7a2725e0e51cef7315c3
florian <74260396+nakotw@users.noreply.github.com>
2024-04-10 15:54:39 -0400

    Delete WINGET UPDATE directory

--- WINGET UPDATE/check.ps1
+++
@@ -1,6 +0,0 @@
-$schtaskName = "Windows Package Manager - UPDATER"
-$Version = "1"
-$taskExists = Get-ScheduledTask | Where-Object {$_.TaskName -like $schtaskName }
-if($taskExists -and ($taskExists.Description -like "*V$Version*")) {
-    Write-Host "Found it!"
-}
